169e85754f85d9c3463c0d1fe5ef97dfdd7d0dfd
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-02-18 16:06:54 +0100

    Enable extending of form

--- static/js/learners_form.js
+++ static/js/learners_form.js
@@ -1,7 +1,38 @@
 $(function () {
 
-    $("#add-input-row").click(() => {
-        console.log("clicked");
+    $("#add-input-row").click(function() {
+
+        let current_id = $(this).attr('value');
+
+        while ($("#inputgroup_" + current_id).length) {
+            current_id ++;
+        }
+
+        var html = '';
+        html += '<div id="inputgroup_' + current_id + '" class="input-group">';
+        html += $("#inputgroup_" + (current_id - 1)).html()
+        html += '</div>';
+
+        $("#additionalInput").append(html);
+        $("#additionalInput h4").html(
+            "Additional " + 
+            $("#inputgroup_" + $(this).attr('value') + " h4").html()
+        );
+
+        // let group_id = $(this).attr('value');
+        // let current_group = $("#" + group_id)[0];
+
+        // let new_group = current_group.append("<div>asdasdda</div>");
+        // current_group.html(new_group);
+
+
+        // let form = $(this).closest("form");
+        // let current_content = form.html();
+
+        // // form.append()
+        
+        // console.log(group_id);
+        // console.log($("#" + group_id)[0]);
     })
 
 });
