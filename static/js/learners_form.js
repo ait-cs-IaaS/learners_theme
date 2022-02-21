73cfcf0e3c6be8c590c564512d5c935eef254aba
Lenhard Reuter <lenhard.reuter@ait.ac.at>
2022-02-21 16:38:20 +0100

    Add form javascript functionalities

--- static/js/learners_form.js
+++ static/js/learners_form.js
@@ -1,38 +1,111 @@
 $(function () {
 
-    $("#add-input-row").click(function() {
+    // window.localStorage.setItem(
+    //     "formbuffer", 
+    //     $("form").html()
+    //     );
 
-        let current_id = $(this).attr('value');
+    // $("#additionalInput").html(window.localStorage.getItem("formbuffer"));
 
-        while ($("#inputgroup_" + current_id).length) {
-            current_id ++;
+    // maybe persist form data in local storage?
+    // https://www.section.io/engineering-education/how-to-use-localstorage-with-javascript/
+
+    $.extend(jQuery.validator.messages, {
+        required: "*"
+    });
+
+    
+    $.each( $(".form #inputgroup_"), function( index){
+        let new_index = index + 1;
+
+        $(this).attr("id", "inputgroup_" + new_index);
+        $(this).parent().find(".add-input-row").attr("value", "inputgroup_" + new_index);
+        
+        replace_identifiers($(this), new_index);
+
+    });
+
+    $(".add-input-row").click(function() {
+
+        let current_id = $(this).attr("value");
+        let next_index = parseInt(current_id.match(/\d+/)[0], 10) * 100;
+        let additional_container = $("#" + current_id).parent().find("#additionalInput")
+
+        let last_item = additional_container.find(".input-group").last()
+        if (last_item.length != 0) {
+            next_index = parseInt((last_item.attr("id")).match(/\d+/)[0], 10) + 1;
         }
+        
+        let new_html_object = $("#" + current_id).clone();
+        replace_identifiers(new_html_object, next_index);
 
-        var html = '';
-        html += '<div id="inputgroup_' + current_id + '" class="input-group">';
-        html += $("#inputgroup_" + (current_id - 1)).html()
+        let html = '<div id="inputgroup_' + next_index + '" style="display: none" class="input-group">';
+        html += '<div class="closer"><svg class="bi" width="50%" height="50%"><use xlink:href="#close"></use></svg></div>';
+        html += new_html_object.html()
         html += '</div>';
 
-        $("#additionalInput").append(html);
-        $("#additionalInput h4").html(
+        additional_container.append(html);
+        additional_container.find("h4").html(
             "Additional " + 
-            $("#inputgroup_" + $(this).attr('value') + " h4").html()
+            $("#" + current_id + " h4").html()
         );
 
-        // let group_id = $(this).attr('value');
-        // let current_group = $("#" + group_id)[0];
+        $("#inputgroup_" + next_index).slideDown();
 
-        // let new_group = current_group.append("<div>asdasdda</div>");
-        // current_group.html(new_group);
+        $(document).on("click", ".closer" , function() {
+            $(this).parent().slideUp("normal", function() { 
+                $(this).remove(); 
+            });
+        });
 
-
-        // let form = $(this).closest("form");
-        // let current_content = form.html();
-
-        // // form.append()
-        
-        // console.log(group_id);
-        // console.log($("#" + group_id)[0]);
     })
 
+    $( ".form" ).validate({
+        rules: getValidationRules(),
+        submitHandler: function(form) {
+            console.log($(form));
+        }        
+    })
+    
+
+    // $( ".form" ).submit(function(event) {
+    //     event.preventDefault();
+    //     rule_dict = getValidationRules()[0];
+    //     msg_dict = getValidationRules()[1];
+        
+    //     // $.post(
+    //     //     'insert_endpoint_here.html',
+    //     //      $(this).serialize(),
+    //     //      function(data){
+    //     //        $("#results").html(data)
+    //     //      }
+    //     //    );
+    //     //    return false;   
+    //   });
+
 });
+
+function replace_identifiers(obj, next_index) {
+    let input_types = ["input", "textarea", "select"]
+
+    $.each(input_types, function(){
+        $.each( obj.find(String(this)), function(){
+            console.log(this)
+            $(this).attr("id", $(this).attr("id") + "_" + next_index);
+            $(this).attr("name", $(this).attr("name") + "_" + next_index);
+        });
+    });
+
+    $.each( obj.find("label"), function(){
+        $(this).attr("for", $(this).attr("for") + "_" + next_index);
+    });
+}
+
+function getValidationRules() {
+    var rule_dict = {}
+    $.each( $(".form .required"), function(){
+        key = $(this).attr("id");
+        rule_dict[key] = "required";
+    });
+    return rule_dict
+}
